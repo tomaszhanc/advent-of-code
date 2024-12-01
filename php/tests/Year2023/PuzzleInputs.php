60390f861eb98a9ff73767cf0d347131365d0271
Tomasz Hanc <7013293+tomaszhanc@users.noreply.github.com>
2024-12-01 07:27:09 +0100

    Preparation for 2024

--- php/tests/Year2023/PuzzleInputs.php
+++ php/tests/Year2023/PuzzleInputs.php
@@ -11,56 +11,56 @@ final readonly class PuzzleInputs
 {
     public static function day1_calibrationDocument(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day1.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day1.txt');
     }
 
     public static function day2_gamesRecord(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day2.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day2.txt');
     }
 
     public static function day3_engineSchematic(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day3.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day3.txt');
     }
 
     public static function day4_scratchpadsRecord(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day4.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day4.txt');
     }
 
     public static function day5_almanac(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day5.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day5.txt');
     }
 
     public static function day6_sheetOfPaper(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day6.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day6.txt');
     }
 
     public static function day6_sheetOfPaperWithFixedKerning(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day6_fixed_kerning.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day6_fixed_kerning.txt');
     }
 
     public static function day7_listOfHands(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day7.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day7.txt');
     }
 
     public static function day8_navigation_document(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day8.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day8.txt');
     }
 
     public static function day9_oasis_report(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day9.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day9.txt');
     }
 
     public static function day10_pipe_diagram(): Input
     {
-        return FileInput::fromLocal(__DIR__ . '/../../resources/day10.txt');
+        return FileInput::fromLocal(__DIR__ . '/../../resources/2023/day10.txt');
     }
 }
