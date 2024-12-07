9fe66d93228cf59cd5059da05588e769f137daae
Tomasz Hanc <7013293+tomaszhanc@users.noreply.github.com>
2024-12-07 17:55:33 +0100

    Add tests for Day 7;

---
+++ js/tests/2024/day07.example.test.ts
@@ -0,0 +1,26 @@
+import {Input} from "../../src/shared/Input";
+import {totalCalibrationValues} from "../../src/2024/day07/solution/part1";
+import {totalCalibrationValuesFor3operators} from "../../src/2024/day07/solution/part2";
+import { describe, expect, it } from 'vitest'
+
+const input = new Input(`
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+`);
+
+describe('AoC 2024, Day 7, Example', () => {
+  it('sums all valid test values that are possible to calculate using operators "+" and "*"', () => {
+    expect(totalCalibrationValues(input)).toBe(3749)
+  })
+
+  it('sums all valid test values that are possible to calculate using operators "+", "*" and "*"', () => {
+    expect(totalCalibrationValuesFor3operators(input)).toBe(11387)
+  })
+});
---
+++ js/tests/2024/day07.test.ts
@@ -0,0 +1,16 @@
+import {readPuzzleInput} from "../../src/shared/Input";
+import {totalCalibrationValues} from "../../src/2024/day07/solution/part1";
+import {totalCalibrationValuesFor3operators} from "../../src/2024/day07/solution/part2";
+import { describe, expect, it } from 'vitest'
+
+const input = readPuzzleInput('2024/day7.txt')
+
+describe('AoC 2024, Day 7, Example', () => {
+  it('sums all valid test values that are possible to calculate using operators "+" and "*"', () => {
+    expect(totalCalibrationValues(input)).toBe(28730327770375)
+  })
+
+  it('sums all valid test values that are possible to calculate using operators "+", "*" and "*"', () => {
+    expect(totalCalibrationValuesFor3operators(input)).toBe(424977609625985)
+  })
+});
