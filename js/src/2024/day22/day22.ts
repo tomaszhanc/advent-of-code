b219da19100a5d495c3a90340afe0f5f0e4ea2b2
Tomasz Hanc <7013293+tomaszhanc@users.noreply.github.com>
2024-12-24 13:05:58 +0100

    2024, Day 22, Part 1

--- js/src/2024/day22/day22.ts
+++ js/src/2024/day22/day22.ts
@@ -1,14 +1,18 @@
-import {Grid} from "../../shared/grid/Grid";
 import {readByLine} from "../../shared/read.input";
 
-export function part1(input: string): number[] {
-    // const _ = parsePuzzleInput(input);
-    const secret = +input;
+export function part1(input: string): number {
+    const secretNumbers = parsePuzzleInput(input);
+    let sum = 0;
 
-    const next = nextSecretNumber(secret);
-    const nextnext = nextSecretNumber(next);
+    for (let secret of secretNumbers) {
+        for (let i = 0; i < 2000; i++) {
+            secret = nextSecretNumber(secret);
+        }
 
-    return [next, nextnext, nextSecretNumber(nextnext)];
+        sum += secret;
+    }
+
+    return sum;
 }
 
 export function part2(input: string): number {
@@ -17,36 +21,15 @@ export function part2(input: string): number {
     return 0;
 }
 
-function parsePuzzleInput(input: string) {
-    return Grid.fromArray(readByLine(input).map(line => line.split('')));
+function parsePuzzleInput(input: string) : number[] {
+    return readByLine(input).map(Number);
 }
 
-function nextSecretNumber(x: number) {
-    // let a = secret*64 ^ secret;
-    //
-    //
-    // const mix = (secret: number, other: number) => secret ^ other;
-    // const prune = (secret: number) => secret % 16777216;
-    //
-    // const part1 = prune(mix(secret, secret * 64));
-    // const part2 = prune(mix(part1, Math.floor(part1 / 32)));
-    // const part3 = prune(mix(part2, part2 * 2048));
+function nextSecretNumber(secret: number) : number {
+    const mix = (secret: number, other: number) => secret ^ other;
+    const prune = (secret: number) => secret & 0xFFFFFF // = secret % 2^24 = secret % 16777216
 
-    const K = 16777216; // Modulo value
-
-    // Step 1: Calculate a
-    const a = (65 * x) % K;
-
-    // Step 2: Calculate q (ceil(a / 32))
-    const q = Math.ceil(a / 32);
-
-    // Step 3: Calculate b
-    const b = (q ^ a) % K;
-
-    // Step 4: Calculate c
-    const c = ((b * 2048) ^ b) % K;
-
-    return c;
-
-    return part3;
+    const part1 = prune(mix(secret, (secret << 6)));
+    const part2 = prune(mix(part1, part1 >> 5));
+    return prune(mix(part2, part2 << 11));
 }
--- js/tests/2024/day22.test.ts
+++ js/tests/2024/day22.test.ts
@@ -1,25 +1,34 @@
 import {describe, expect, it} from "vitest";
 import {readPuzzleInput} from "../../src/shared/read.input";
-import {mix, part1, part2, prune} from "../../src/2024/day22/day22";
+import {part1, part2} from "../../src/2024/day22/day22";
 
-const example = `
-
-`;
 const input = readPuzzleInput('2024/day22.txt');
+const examplePart1 = `
+1
+10
+100
+2024
+`;
+const examplePart2 = `
+1
+2
+3
+2024
+`;
 
 describe('AoC 2024, Day 22, Part 1', () => {
     it('checks the example', () => {
-        expect(part1('123')).toBe(15887950)
+        expect(part1(examplePart1)).toBe(37327623)
     })
 
     it('check the input', () => {
-        expect(part1(input)).toBe(-1)
+        expect(part1(input)).toBe(18941802053)
     })
 });
 
 describe('AoC 2024, Day 22, Part 2', () => {
     it('checks the example', () => {
-        expect(part2(example)).toBe(-1)
+        expect(part2(examplePart2)).toBe(23)
     })
 
     it('checks the input', () => {
